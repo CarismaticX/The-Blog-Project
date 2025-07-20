e9fc5cae6129c20a650f809600b0c19ce294eaa4
Shekhar Singh Kushwah <kushwahshekhar.singh44@gmail.com>
2025-07-20 22:45:26 +0530

    Fix: re-add client as normal folder after removing submodule

---
+++ client/src/components/Post.jsx
@@ -0,0 +1,29 @@
+
+function Post(){
+return (
+<div className='post'>
+        <div className='image'>
+          <img
+            src='https://techcrunch.com/wp-content/uploads/2024/07/GettyImages-1917904267.jpg?resize=1280,854'
+            alt=''
+          />
+        </div>
+        <div className='texts'>
+          <h2>Perplexity sees India as a shortcut in its race against OpenAI</h2>
+          <p className='info'>
+            <a href='' className='author'>Shekhar Kushwah</a>
+            <time dateTime='2025-07-18T13:07'>18-07-25 13:07</time>
+          </p>
+          <p className='summary'>
+            While OpenAI has cemented its lead in the U.S., Perplexity is taking a different route —
+            quietly expanding into India to compete in the next phase of AI adoption. The
+            search-focused AI startup is rapidly adding millions of users in the world’s
+            second-largest internet and smartphone market, positioning itself for mass-market scale.
+          </p>
+        </div>
+      </div>
+
+)
+}
+
+export default Post
