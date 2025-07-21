5f23efc92c3749bb4734f33b316169d6342e7440
Shekhar Singh Kushwah <kushwahshekhar.singh44@gmail.com>
2025-07-21 22:54:39 +0530

    added Authentication and connected to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,38 @@
 import express from 'express';
+import cors from "cors";
+import mongoose from 'mongoose';
+import User from './models/User.js';
+import bcrypt from "bcryptjs";
 const app = express();
 
+const salt = bcrypt.genSaltSync(10);
 
-app.get('/register' ,(req,res) => {
-    res.json('test okay');
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://Shekhar:Shekhar123@my-blog.ypqpfgu.mongodb.net/?retryWrites=true&w=majority&appName=My-Blog')
+
+
+app.post('/register' ,async(req,res) => {
+    const{username, password} = req.body;
+    try{
+    const userDoc = await User.create({username, 
+        password:bcrypt.hashSync(password, salt)})
+    res.json(userDoc);
+    }catch(err){
+        res.status(400).json(err)
+    }
+})
+
+
+app.post('/login', async(req,res) =>{
+    const{username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    console.log(passOk);
+
 })
 
 app.listen(4000);
+
+//mongodb+srv://Shekhar:Shekhar123@my-blog.ypqpfgu.mongodb.net/?retryWrites=true&w=majority&appName=My-Blog
---
+++ api/models/User.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, minLength: 4, unique: true },
+  password: { type: String, required: true }
+});
+
+const UserModel = model("User", UserSchema);
+
+export default UserModel;
